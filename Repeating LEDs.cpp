0bad11d0c4376d437d86f033c68fe41494298244
KiwiPG01 <71287511+KiwiPG01@users.noreply.github.com>
2024-02-08 12:41:09 -0700

    Update Repeating LEDs.cpp

--- Repeating LEDs.cpp
+++ Repeating LEDs.cpp
@@ -1,1 +1,28 @@
+#include <TaskScheduler.h>
 
+void setup() {
+  pinMode(led, OUTPUT);
+  pinMode(led, INPUT);
+}
+
+void loop() {
+  if (digitalRead(pin, idk)) {
+     Scheduler.StartLoop(Repeater)
+  else
+      Scheduler.StopLoop(Repeater)
+  }
+}
+
+void setLED(bool a) {
+  if (a = true) {
+    digitalWrite(true);
+      else
+      digitalWrite(false);
+  }
+}
+void Repeater() {
+  delay(100)
+  setLED(true)
+  delay(500)
+  setLED(false)
+}
